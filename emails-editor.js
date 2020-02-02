523fb36db4fa00f7ea925da45ed136b157cc2b36
Luisfer Romero Calero <luisfer.romero.calero@gmail.com>
2020-02-02 21:31:12 +0100

    Added styling to match designs, created right scope for the plugin, general refactoring

--- emails-editor.js
+++ emails-editor.js
@@ -2,167 +2,220 @@
 // TODOS:
 /*
 ✅ 0. Create git repository
-0.1 Create box function
 ✅ 1. Create event listeners with enter, comma.
 ✅ 2. Create blocks. A block can be deleted.
 ✅ 3. Create width to match parent.
 4. Create scrollable textarea.
 ✅ 5. If email is not valid, the block is red.
 ✅ 6. Create button 'add email' that adds a random email (random email generator).
-7. Create button 'get emails count' for a user to see an alert with ONLY valid emails count.
-8. Create support for creating several emails editors in the DOM.
-9. Make sure I can use it as a plugin elsewhere
+✅ 7. Create button 'get emails count' for a user to see an alert with ONLY valid emails count.
+✅ 8. Create support for creating several emails editors in the DOM.
 ✅  10. Bundle .css in .js file
-11. Match design of the box
-12. Add support for control+v and split commas
-12.1. Add support for losing focus
+✅ 11. Match design of the box
+✅ 12. Add support for control+v and split commas
+✅ 12.1. Add support for losing focus
 13. Think of options ('API calls') and include them
 14. Review and refine
 15. Document options
+
+
+BUGS:
+✅ 1. Deleting an email doesn't work
+2. Both container share the same emails
+
+
+NICE TO HAVES:
+1. Put in local storage
+
 */
 
-class EmailsStore {
-	constructor(emails){
-		this.emails = emails || [];
-	}
+(function (root, factory) {
+  'use strict';
 
-	getEmails = () => {
-		return this.emails;
-	}
+  if (typeof define === 'function' && define.amd) {
 
-	setEmails = (arr) => {
-		console.log(this.emails, arr);
-		this.emails = [...this.emails,...arr];
-		console.log(this.emails, arr);
-	}
+		define([], function () {
+      return (root.EmailsEditor = factory());
+    });
+  } else if (typeof exports === 'object') {
 
-  addEmails = (emailContainer) => {
-		this.clearEmails();
-	  this.emails.slice().reverse().forEach(email => {
-	    emailContainer.prepend(createEmail(email));
-	  });
-	}
+		module.exports = factory();
+  } else {
 
-	removeEmail = (index) => {
-		this.emails = [...this.emails.slice(0, index), ...this.emails.slice(index+1)];
-	}
+		root.EmailsEditor = factory();
+  }
+})(this, function () {
 
-	clearEmails = () => {
-		document.querySelectorAll('.email').forEach(email => {
-	    email.parentElement.removeChild(email);
-	  });
-	}
+  'use strict';
 
-	getValidEmails = () => {
-		return this.emails.filter((email) => validateEmail(email) == true);
-	}
-}
+	class EmailsStore {
+		constructor(id){
+			this.emails = [];
+			this.id = id;
+		}
 
-var store = new EmailsStore([]);
+		setContainer = (container) => {
+			this.container = container;
+		}
 
-var EmailsEditor = function(args){
+		getEmails = () => {
+			return this.emails;
+		}
+
+	  addEmail = (email) => {
+			this.emails.push(email);
+			this.updateContainer();
+		}
+
+		removeEmail = (index) => {
+			this.emails = [...this.emails.slice(0, index), ...this.emails.slice(index+1)];
+			this.updateContainer();
+		}
+
+		updateContainer = () => {
+			this.clearEmails();
+		  this.emails.slice().reverse().forEach(email => {
+		    this.container.prepend(createEmail(email, this.id));
+		  });
+		}
+
+		clearEmails = () => {
+			let emailClass = '.email__' + this.id;
+			document.querySelectorAll(emailClass).forEach(email => {
+		    email.parentElement.removeChild(email);
+		  });
+		}
+
+		getValidEmails = () => {
+			return this.emails.filter((email) => validateEmail(email) == true);
+		}
+	};
+
+	var stores = [];
+
+  var EmailsEditor = function(args){
+		let uuid = uuidv4();
+		stores.push({id: uuid, store: new EmailsStore(uuid)});
+		console.log(stores);
 
 		args.container.setAttribute('class', 'container');
 
 		let emailsContainer = document.createElement("div");
+		emailsContainer.setAttribute('id', 'container__' + uuid);
 		emailsContainer.setAttribute('class', 'emails-container');
+
+		let currentStore = stores.find(x => x.id === uuid).store;
+		currentStore.setContainer(emailsContainer);
+
 		let emailsContainerTitle = document.createElement("div");
 		emailsContainerTitle.setAttribute('class', 'emails-container-title');
-		emailsContainerTitle.innerHTML = "Share <b>Board Name</b> with others";
+		emailsContainerTitle.innerHTML = "Share <span class=\"bold\">Board Name</span> with others";
 		let input = document.createElement("input");
 		input.placeholder = "Add more people...";
-
-		var emails = new EmailsStore([]);
 
 		input.addEventListener('keyup', (e) => {
 		    if (e.key === 'Enter') {
 					if (e.target.value.toString().trim() !== ""){
 						e.target.value.split(',').forEach(email => {
-			        store.getEmails().push(email);
+			        currentStore.addEmail(email);
 			      });
-			      store.addEmails(emailsContainer);
 			      input.value = '';
 					}
 		    }
 		});
 
+		input.addEventListener('blur', (e) => {
+			if (e.target.value.toString().trim() !== ""){
+				e.target.value.split(',').forEach(email => {
+					currentStore.addEmail(email);
+				});
+				input.value = '';
+			}
+		})
+
 		document.addEventListener('click', (e) => {
-			console.log(e.target);
-		  console.log("tagName", e.target.tagName);
-		  if (e.target.tagName === 'I') {
+			if (e.target.tagName === 'I') {
 		    const emailLabel = e.target.getAttribute('data-item');
-		    const index = store.getEmails().indexOf(emailLabel);
-				store.removeEmail(index);
-				console.log(store.getEmails());
-		    store.addEmails(emailsContainer);
+		    const index = currentStore.getEmails().indexOf(emailLabel);
+				currentStore.removeEmail(index);
 		  }
 		})
 
-		let addEmailButton = createAddEmailButton(emailsContainer);
-		let getEmailCountButton = createGetEmailCountButton(emailsContainer);
+		let addEmailButton = createAddEmailButton(currentStore);
+		let getEmailCountButton = createGetEmailCountButton(currentStore);
 
 		emailsContainer.appendChild(input);
 		args.container.appendChild(emailsContainerTitle);
 		args.container.appendChild(emailsContainer);
-		args.container.appendChild(addEmailButton);
-		args.container.appendChild(getEmailCountButton);
+
+		let buttonsContainer = document.createElement("div");
+		buttonsContainer.setAttribute('class', 'buttons-container');
+
+		buttonsContainer.appendChild(addEmailButton);
+		buttonsContainer.appendChild(getEmailCountButton);
+		args.container.appendChild(buttonsContainer);
 		input.focus();
-
-		return true;
-
-};
-
-var createAddEmailButton = function(emailsContainer){
-	let addEmailButton = document.createElement("button");
-	addEmailButton.innerHTML = "Add Email";
-	addEmailButton.addEventListener('click', (e) => {
-		store.setEmails([generateRandomEmail()])
-		store.addEmails(emailsContainer);
-	});
-	return addEmailButton;
-}
-
-var createGetEmailCountButton = function(emailsContainer){
-	let getEmailCountButton = document.createElement("button");
-	getEmailCountButton.innerHTML = "Get Email Count";
-	getEmailCountButton.addEventListener('click', (e) => {
-		alert(store.getValidEmails().length);
-	});
-	return getEmailCountButton;
-}
-
-
-var helper = function(){
-
-}
-
-function createEmail(label) {
-  const div = document.createElement('div');
-	let isValid = validateEmail(label);
-	if (isValid){
-		div.setAttribute('class', 'email');
-	} else {
-		div.setAttribute('class', 'email email--invalid');
 	}
-  const span = document.createElement('span');
-  span.innerHTML = label;
-  const closeIcon = document.createElement('i');
-  closeIcon.innerHTML = 'close';
-  closeIcon.setAttribute('class', 'material-icons');
-  closeIcon.setAttribute('data-item', label);
-  div.appendChild(span);
-  div.appendChild(closeIcon);
-  return div;
-}
 
-function validateEmail(email) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+	var createAddEmailButton = function(currentStore){
+		let addEmailButton = document.createElement("button");
+		addEmailButton.innerHTML = "Add email";
+		addEmailButton.addEventListener('click', (e) => {
+			currentStore.addEmails([generateRandomEmail()])
+		});
+		return addEmailButton;
+	}
+
+	var createGetEmailCountButton = function(currentStore){
+		let getEmailCountButton = document.createElement("button");
+		getEmailCountButton.innerHTML = "Get email count";
+		getEmailCountButton.addEventListener('click', (e) => {
+			alert("Valid emails: " + currentStore.getValidEmails().length);
+		});
+		return getEmailCountButton;
+	}
 
 
-function generateRandomEmail(){
-	// Needs work. First letter has to be a char
-	var chars = 'abcdefghijklmnopqrstuvwxyz';
-	return chars[Math.floor(Math.random()*26)] + Math.random().toString(36).substring(2,11) + '@randomemail.com';
-}
+	var helper = function(){
+
+	}
+
+	function createEmail(label, uuid) {
+	  const div = document.createElement('div');
+		let isValid = validateEmail(label);
+		if (isValid){
+			div.setAttribute('class', 'email ' + 'email__' + uuid);
+		} else {
+			div.setAttribute('class', 'email email--invalid ' + 'email__' + uuid);
+		}
+	  const span = document.createElement('span');
+	  span.innerHTML = label;
+	  const closeIcon = document.createElement('i');
+	  closeIcon.innerHTML = 'close';
+	  closeIcon.setAttribute('class', 'material-icons');
+	  closeIcon.setAttribute('data-item', label);
+	  div.appendChild(span);
+	  div.appendChild(closeIcon);
+	  return div;
+	}
+
+	function validateEmail(email) {
+	    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	    return re.test(String(email).toLowerCase());
+	}
+
+	function generateRandomEmail(){
+		var chars = 'abcdefghijklmnopqrstuvwxyz';
+		return chars[Math.floor(Math.random()*26)] + Math.random().toString(36).substring(2,11) + '@randomemail.com';
+	}
+
+	function uuidv4() {
+	  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+	    return v.toString(16);
+	  });
+	}
+
+
+	return EmailsEditor;
+});
